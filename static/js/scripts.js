9d13cc9e60ef104a0f1cd6a6b730194b680b819b
Pedro <pedrohs21@outlook.com>
2020-07-21 23:28:11 -0300

    Implementação da tipografia alternada por letras e palavras.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -50,6 +50,7 @@ function finder(text, str) { // Recebe duas strings(text e str) e retorna os ín
 function subsStr(str, index, subs) { // Substitui uma substring em uma string, recebe a string, o indice onde será substituido e a substring. 
 // Falta tratar quando não recebe os argumentos, ou eles são do tipo errado.
 // Quando passa os índices finais retorna uma string maior
+    intex = parseInt(index);
     return index >= str.length ? str : str.substr(0, index) + subs + str.substr(index + subs.length, str.length - 1);
 }
 
@@ -65,8 +66,7 @@ function upperPhrases() { // Coloca a primeira letra de uma frase em maiúscula,
     var endPh = [];
 
     for(let i in separators) {
-        endPh = finder(text, separators[i].text);
-        console.log(endPh, separators[i].text)
+        endPh = finder(text, separators[i].text);        
         for(let j in endPh) {
             let index = endPh[j] + separators[i].len; // O endPh[j] tem o índice do separador, mas o que desejamos substituir é a primeira letra após esse separador por isso a soma.          
             text = subsStr(text, index, text[index].toUpperCase());
@@ -87,7 +87,7 @@ function upperWords() {
     spaces = finder(text, ' ');
 
     for(var i in spaces) {
-        let index = spaces[i] + 1;             
+        let index = spaces[parseInt(i)] + 1;             
         text = subsStr(text, index, text[index].toUpperCase());
     }
 
@@ -96,7 +96,47 @@ function upperWords() {
     setText(text);
 }
 
+function alternateLetters() {
+    var text = getText();
+
+    for(var i in text) {
+        if(parseInt(i) % 2 == 0) {
+            text = subsStr(text, parseInt(i), text[parseInt(i)].toUpperCase());
+        }        
+    }
+
+    setText(text);
+}
+
+function alternateWords() {
+    var text;
+    var spaces;
+    
+    text = getText();
+    spaces = finder(text, ' ');
+
+    for(var i in spaces) {
+        let index = spaces[parseInt(i)] + 1;
+       
+        if(parseInt(i) % 2 == 0) {
+            let tam;
+
+            if(parseInt(i) == spaces.length - 1) {
+                tam = text.length - spaces[parseInt(i)] - 1;                
+            } else {
+                tam = spaces[parseInt(i) + 1] - spaces[parseInt(i)] - 1;
+            }
+
+            text = subsStr(text, index, text.substr(index, tam).toUpperCase());        
+        }           
+    }
+
+    setText(text);
+}
+
 document.getElementById('btn-1').addEventListener('click', upper);
 document.getElementById('btn-2').addEventListener('click', lower);
 document.getElementById('btn-3').addEventListener('click', upperWords);
 document.getElementById('btn-4').addEventListener('click', upperPhrases);
+document.getElementById('btn-5').addEventListener('click', alternateLetters);
+document.getElementById('btn-6').addEventListener('click', alternateWords);
